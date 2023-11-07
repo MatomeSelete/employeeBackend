5767adc082f6c567496c4d9312a432264b758866
MatomeSelete <seletematome@gmail.com>
2023-11-07 10:57:07 +0200

    able to get all  from db

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ const studentRoutes = require('./src/students/routes');
 const app = express();
 const port = 3001;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
     res.send("Hello world!")
 });
+
+app.use('/api/v1/students', studentRoutes);
 
 app.listen(port, () => console.log(`app listing on port ${port}`));
